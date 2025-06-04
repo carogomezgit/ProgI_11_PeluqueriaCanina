0f1ba4824733b89a8eb728c043050273193f3609
Carolina Gomez <aragomezgd@gmail.com>
2025-06-04 18:04:40 -0300

    Actualizar clases

--- src/model/Mascota.java
+++ src/model/Mascota.java
@@ -2,88 +2,87 @@ package model;
 
 public class Mascota {
 
-	// atributos
-	private int id;
-	private static int idCount;
-	private String nombre;
-	private int edad;
-	private Especie especie;
-	private String raza;
-	private String duenio;
+    // atributos
+    private int id;
+    private static int idCount;
+    private String nombre;
+    private int edad;
+    private Especie especie;
+    private String raza;
+    private String duenio;
 
 
-	// metodo constructor
-	public Mascota(String nombre, int edad, Especie especie, String raza, String duenio) {
-		this.id = ++idCount;
-		this.nombre = nombre;
-		this.edad = edad;
-		this.especie = especie;
-		this.raza = raza;
-		this.duenio = duenio;
-	}
+    // metodo constructor
+    public Mascota(String nombre, int edad, Especie especie, String raza, String duenio) {
+        this.id = ++idCount;
+        this.nombre = nombre;
+        this.edad = edad;
+        this.especie = especie;
+        this.raza = raza;
+        this.duenio = duenio;
+    }
 
-	// setters and getters para cada atributo
-	public int getId() {
-		return id;
-	}
+    // setters and getters para cada atributo
+    public int getId() {
+        return id;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public String getNombre() {
-		return nombre;
-	}
+    public String getNombre() {
+        return nombre;
+    }
 
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
-	public int getEdad() {
-		return edad;
-	}
+    public int getEdad() {
+        return edad;
+    }
 
-	public void setEdad(int edad) {
-		this.edad = edad;
-	}
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
 
-	public Especie getEspecie() {
-		return especie;
-	}
+    public Especie getEspecie() {
+        return especie;
+    }
 
-	public void setEspecie(Especie especie) {
-		this.especie = especie;
-	}
+    public void setEspecie(Especie especie) {
+        this.especie = especie;
+    }
 
-	public String getRaza() {
-		return raza;
-	}
+    public String getRaza() {
+        return raza;
+    }
 
-	public void setRaza(String raza) {
-		this.raza = raza;
-	}
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
 
-	public String getDuenio() {
-		return duenio;
-	}
+    public String getDuenio() {
+        return duenio;
+    }
 
-	public void setDuenio(String duenio) {
-		this.duenio = duenio;
-	}
+    public void setDuenio(String duenio) {
+        this.duenio = duenio;
+    }
 
 
-	// metodos
-	@override
-	public String toStringMascota() {
-		return
-				"\n Nombre de mascota: " + this.nombre +
-						"\n Edad: " + this.edad +
-						"\n Especie " + this.especie +
-						"\n Raza: " + this.raza +
-						"\n Dueño: " + this.duenio;
-	}
+    // metodos
+    @override
+    public String toStringMascota() {
+        return "\n Nombre de mascota: " + this.nombre +
+                "\n Edad: " + this.edad +
+                "\n Especie " + this.especie +
+                "\n Raza: " + this.raza +
+                "\n Dueño: " + this.duenio;
+    }
 
-	public void mostrarMascota() {
-		System.out.println(toStringMascota());
-	}
+    public void mostrarMascota() {
+        System.out.println(toStringMascota());
+    }
 }
